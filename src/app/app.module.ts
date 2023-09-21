4caa9a2c2668d48d747eb481720469c429d4cf4b
DESKTOP-WIN10PR\mtysf <mtysfuji@gmail.com>
2023-09-22 01:29:59 +0900

    フロントエンドとバックエンドの結合

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductListComponent } from './product/product-listings/product-listings.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 
@@ -14,6 +15,7 @@ import { ProductDetailComponent } from './product/product-detail/product-detail.
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [],
